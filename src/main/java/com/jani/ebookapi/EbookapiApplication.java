820cac6d68c3100a9b71b66c460357ddb460a3b1
jkjanij <jkjanij@gmail.com>
2023-12-22 14:54:16 +0200

    Project basics, spring initializr generated files

---
+++ src/main/java/com/jani/ebookapi/EbookapiApplication.java
@@ -0,0 +1,13 @@
+package com.jani.ebookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbookapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jani/ebookapi/EbookapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jani.ebookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbookapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
